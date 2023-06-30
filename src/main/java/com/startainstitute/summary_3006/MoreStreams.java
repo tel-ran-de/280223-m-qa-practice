bac17885e0dc2d96b39619bcc8f54dcb390c312d
Anton Ermak <rayman.k26@gmail.com>
2023-06-30 16:25:01 +0700

    Coding problems, streams, builder API.

---
+++ src/main/java/com/startainstitute/summary_3006/MoreStreams.java
@@ -0,0 +1,11 @@
+package com.startainstitute.summary_3006;
+
+
+public class MoreStreams {
+
+    // Даны два списка чисел.
+    // Необходимо найти разность множеств (элементы, присутствующие только в одном из списков)
+    // и вывести результат.
+    public static void main(String[] args) {
+    }
+}
